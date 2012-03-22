179f4ea875008138379c7c1cc9c88fc7fc9cb570
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-22 12:33:22 +0800

    Revert to crm-staging as default deploy

--- config/deploy/ffcrm-staging.rb
+++ config/deploy/ffcrm-staging.rb
@@ -1,8 +1,8 @@
 set :user, "root"
 set :use_sudo, false
 
-set :ip_address, "10.0.1.86"
-set :site_domain_name, "crm-staging.crossroadsint.org"
+set :ip_address, "10.0.1.79"
+set :site_domain_name, "ffcrm-staging.crossroadsint.org"
 set :rails_env, "staging"
 
 server ip_address, :app, :web, :db, :primary => true
@@ -10,3 +10,5 @@ server ip_address, :app, :web, :db, :primary => true
 set :deploy_to, "/opt/rails/#{application}-#{stage}"
 
 set :branch, (ENV['branch'] || "master")
+
+set :repository, "git://github.com/fatfreecrm/ffcrm_app.git"
