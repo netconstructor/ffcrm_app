6f60280f30be5ad18e4e2011aadd76244ad87246
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-22 17:16:20 +0800

    Added mailman daemon tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,3 +99,21 @@ namespace :formstack do
     run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake ffcrm:crossroads:formstack:pull"
   end
 end
+
+namespace :mailman do
+  desc "Mailman::Start"
+  task :start, :roles => [:app] do
+    run "cd #{current_path} && RAILS_ENV=#{rack_env} bundle exec script/mailman_daemon start"
+  end
+
+  desc "Mailman::Stop"
+  task :stop, :roles => [:app] do
+    run "cd #{current_path} && RAILS_ENV=#{rack_env} bundle exec script/mailman_daemon stop"
+  end
+
+  desc "Mailman::Restart"
+  task :restart, :roles => [:app] do
+    mailman.stop
+    mailman.start
+  end
+end
