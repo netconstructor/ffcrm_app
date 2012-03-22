a42507be1e65cdc09a6a92a9e8ffdf386724b4e6
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-22 13:24:42 +0800

    Staging -> ffcrm-staging

--- config/deploy/ffcrm-staging.rb
+++ config/deploy/ffcrm-staging.rb
@@ -4,10 +4,11 @@ set :use_sudo, false
 set :ip_address, "10.0.1.79"
 set :site_domain_name, "ffcrm-staging.crossroadsint.org"
 set :rails_env, "staging"
+set :stage, 'staging'
 
 server ip_address, :app, :web, :db, :primary => true
 
-set :deploy_to, "/opt/rails/#{application}-staging"
+set :deploy_to, "/opt/rails/#{application}-#{stage}"
 
 set :branch, (ENV['branch'] || "master")
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :user, "root"
 set :use_sudo, false
 
 set :ip_address, "10.0.1.86"
-set :site_domain_name, "crm-staging.crossroadsint.org"
+set :site_domain_name, "ffcrm-staging.crossroadsint.org"
 set :rails_env, "staging"
 
 server ip_address, :app, :web, :db, :primary => true
