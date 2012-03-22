f6cd042ad3b6afd446af0661ca5b25284811dba5
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-22 13:46:30 +0800

    Fixed IP address for ffcrm-staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :user, "root"
 set :use_sudo, false
 
-set :ip_address, "10.0.1.86"
+set :ip_address, "10.0.1.79"
 set :site_domain_name, "ffcrm-staging.crossroadsint.org"
 set :rails_env, "staging"
 
