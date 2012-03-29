e0ccf072c7ca0572778979f066c5ab77976a96bb
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-29 14:17:57 +0800

    Explicitly set :application in deploy environments

--- config/deploy/live.rb
+++ config/deploy/live.rb
@@ -4,6 +4,8 @@ set :use_sudo, false
 set :ip_address, "192.168.0.149"
 set :site_domain_name, "crm.crossroads.org.hk"
 
+set :application,  "crm"
+
 server ip_address, :app, :web, :db, :primary => true
 
 set :deploy_to, "/opt/rails/#{application}-#{stage}"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,8 @@ set :ip_address, "10.0.1.79"
 set :site_domain_name, "ffcrm-staging.crossroadsint.org"
 set :rails_env, "staging"
 
+set :application,  "ffcrm"
+
 server ip_address, :app, :web, :db, :primary => true
 
 set :deploy_to, "/opt/rails/#{application}-#{stage}"
