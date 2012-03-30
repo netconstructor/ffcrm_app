a03ab4ca67f44b18feea486a161317eda13fed56
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-30 13:39:37 +0800

    Updated whenever tasks (email tasks now support dry_run for staging environment)

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,35 +3,28 @@
 # Set up email notifications for errors. Airbrake can only send us errors once the environment has initialized.
 env :MAILTO, 'itdept@crossroads.org.hk'
 
-formstack_start, dropbox_start, comments_start = (environment == 'staging') ? [5, 3, 0] : [7, 5, 2]
-
 # Turns (3) into "3,13,23,33,43,53"
-def start_min_to_string(start, interval = 10)
+# If the environment is production, add two minutes to the start time
+# so that staging can process the emails/submissions first without deleting them
+def start_min_to_cron(start, interval = 10)
+  start += 2 if environment == 'production'
   ((start)..(start + 60)).step(interval).to_a.reject{|i| i > 59 }.join(',')
 end
 
 job_type :rake, "cd :path > /dev/null && RAILS_ENV=:environment bundle exec rake :task --silent :output"
 
-unless environment == 'staging'
-  # The following :output conf prints STDERR (and receive emails from cron), and saves STDOUT to log
-  every "#{start_min_to_string(formstack_start)} 7-23 * * *" do
-    # Formstack submissions
-    rake "ffcrm:crossroads:formstack:pull", :output => {:standard => "log/formstack_cron.log"}
-  end
+
+every "#{start_min_to_cron(5)} 7-23 * * *" do
+  # Formstack submissions
+  rake "ffcrm:crossroads:formstack:pull", :output => {:standard => "log/formstack_cron.log"}
 end
 
-#TODO Dropbox currently deletes emails on staging too...
-unless environment == 'staging'
-  every "#{start_min_to_string(dropbox_start)} 7-23 * * *" do
-    # Email dropbox
-    rake "ffcrm:dropbox:run", :output => {:standard => "log/dropbox_cron.log"}
-  end
+every "#{start_min_to_cron(3)} 7-23 * * *" do
+  # Email dropbox
+  rake "ffcrm:dropbox:run", :output => {:standard => "log/dropbox_cron.log"}
 end
 
-
-#TODO Comments Replies Inbox currently deletes emails on staging too...
-every "#{start_min_to_string(comments_start, 2)} * * * *" do
+every "#{start_min_to_cron(0, 4)} * * * *" do
   # Comments Replies Inbox
   rake "ffcrm:comment_replies:run", :output => {:standard => "log/comment_replies_cron.log"}
 end
-
