382f758ddfbfcb7f1a603ccc3bdfd04979a768d2
Steve Kenworthy <steveyken@gmail.com>
2012-05-10 18:33:39 +0800

    Fixed bamboo script

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -8,18 +8,16 @@ yum --quiet -y install `yum_bamboo_packages` # Install required yum packages
 . setup_rvm_environment   ffcrm_app    ruby-1.9.2-p290   1.8.21     1.1.3
 #~ . setup_rvm_environment   ffcrm_app    ruby-1.9.3-p125   1.8.21     1.1.3
 
-# Install bundled gems
-bundle install
-
 RAILS_ENV=test
 plugins="ffcrm_mingle ffcrm_meta_search"
 # run tests on each plugin
 for plugin in $plugins
 do
-  psql -c 'create database $(plugin)_test;' -U postgres
+  createdb -U postgres "$plugin"_test
   cd $plugin
   bundle install
   bundle exec rake db:schema:load
   bundle exec rspec --require ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec
-  psql -c 'drop database $(plugin)_test;' -U postgres
+  dropdb -U postgres "$plugin"_test
+  cd ..
 done
